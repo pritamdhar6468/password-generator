1c06d5032cb40a132eb8296f9d3e478efacad2d6
Pritamdhar_official <pritamdhar6568@gmail.com>
2023-02-27 21:55:28 +0530

    updated page

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-const lengthSlider = document.querySelector(".pass-length input");
+const lengthSlider = document.querySelector(".pass-length input"),
+generateBtn = document.querySelector(".generate-btn");
+
+
+const generatePassword =() =>{
+    let staticPassword = "";
+}
+
 const updateSlider = () =>{
     document.querySelector(".pass-length span").innerText = lengthSlider.Value;
 }
-
+updateSlider();
 lengthSlider.addEventListener("input", updateSlider);
+generateBtn.addEventListener("click", generatePassword);
