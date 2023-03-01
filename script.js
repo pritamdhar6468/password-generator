23c4589d95ef1c9e314e9ea06a73aa71dfb8bd7c
Pritamdhar_official <pritamdhar6568@gmail.com>
2023-03-01 10:54:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const lengthSlider = document.querySelector(".pass-length input"),
+options = document.querySelector(".option input"),
 generateBtn = document.querySelector(".generate-btn");
 
 
 const generatePassword =() =>{
     let staticPassword = "";
+
+    options.forEach(option => {
+        if(option.checked) {
+            console.log(option);
+        }
+    });
 }
 
 const updateSlider = () =>{
