92b3d03444c670a324fdb6dec7b69ae4567cb51a
Pritamdhar_official <pritamdhar6568@gmail.com>
2023-03-02 15:05:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,21 @@ options = document.querySelector(".option input"),
 generateBtn = document.querySelector(".generate-btn");
 
 
+const characters = {
+    lowercase:"abcdefghijklmnopqrstuvwxyz",
+    uppercase:"ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    numbers:"0123456789",
+    symbols:"!~@#$%^&890_=+<>?|",
+}
+
+
 const generatePassword =() =>{
     let staticPassword = "";
 
     options.forEach(option => {
         if(option.checked) {
-            console.log(option);
+            //adding particular key value from character object to staticpassword
+            staticPassword += characters[option.id];
         }
     });
 }
